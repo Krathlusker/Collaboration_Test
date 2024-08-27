a9bddff54010ec4b582be4cbdef84301d0dce2da
Frederik Krath Nielsen <Frederikkrath@gmail.com>
2024-08-27 18:04:52 +0200

    Added product-database, styled basic cart

---
+++ indkøbskurv/products.js
@@ -0,0 +1,62 @@
+const products = [
+    {
+        id: 0,
+        titel: "McLaren P1™",
+        price: 3499.95,
+        stock: 3,
+        info: "",
+        /* If you want offer the product as NEW */
+        OfferNew: 1,
+        /* If you want offer the product as EXCLUSIVE */
+        OfferExclusive: 1,
+        imgpath: "",
+    },
+    {
+        id: 1,
+        titel: "McLaren P1™",
+        price: 3499.95,
+        stock: 3,
+        info: "",
+        /* If you want offer the product as NEW */
+        OfferNew: 1,
+        /* If you want offer the product as EXCLUSIVE */
+        OfferExclusive: 1,
+        imgpath: "",
+    },
+    {
+        id: 2,
+        titel: "McLaren P1™",
+        price: 3499.95,
+        stock: 3,
+        info: "",
+        /* If you want offer the product as NEW */
+        OfferNew: 1,
+        /* If you want offer the product as EXCLUSIVE */
+        OfferExclusive: 1,
+        imgpath: "",
+    },
+    {
+        id: 3,
+        titel: "McLaren P1™",
+        price: 3499.95,
+        stock: 3,
+        info: "",
+        /* If you want offer the product as NEW */
+        OfferNew: 1,
+        /* If you want offer the product as EXCLUSIVE */
+        OfferExclusive: 1,
+        imgpath: "",
+    },
+    {
+        id: 4,
+        titel: "McLaren P1™",
+        price: 3499.95,
+        stock: 3,
+        info: "",
+        /* If you want offer the product as NEW */
+        OfferNew: 1,
+        /* If you want offer the product as EXCLUSIVE */
+        OfferExclusive: 1,
+        imgpath: "",
+    }
+] 
