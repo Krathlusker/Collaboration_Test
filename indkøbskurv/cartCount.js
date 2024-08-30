8a37f4c242e3053ef94b0fbab30d2919af37e424
Frederik Krath Nielsen <Frederikkrath@gmail.com>
2024-08-30 20:24:35 +0200

    added template for items in cartlist
    Expectingto add remaining cartfunctionality the 31. of august 2024:
    - Number of items shown in upper right corner
    - Cartlist / Itemlist generated from localstorage
    - a "Remove" funcionality from cartlist
    - "Empty cart-page"
    - JavaScript-functionality for all above

--- indkøbskurv/cartCount.js
+++ indkøbskurv/cartCount.js
@@ -2,10 +2,13 @@ const cartVisible = document.getElementById('cart-count').innerHTML;
 
 // for (i=0; i < ;)
 
-console.log(localStorage.cart);
+// Viser indholdet af localstorage
+// console.log(localStorage.cart);
 
+// Henter inholdet i localstorage
 let cart = localStorage.getItem('cart');
 
+// Læser indholdet, og tæller hvor mange der er af hvert id og gemmer det i et object 'count'
 if (cart) {
     // 2. Konverter dataen til et array
     cart = cart.split(',').map(Number); // Split stringen ved ',' og konverter til tal
@@ -18,14 +21,15 @@ if (cart) {
         count[value] = (count[value] || 0) + 1;
     });
 
-    console.log(typeof count)
+    // console.log(typeof count)
 
     // 5. Log resultatet    
     console.log(count);
-    // Eksempel output: {0: 2, 1: 3, 2: 1} 
-    // (Dette betyder, at der er to 0'ere, tre 1'ere og én 2'er i cart)
+
 } else {
     console.log("Cart is empty or not found.");
 }
 
-// F*** dig Thomas <3 Funk you Thomas
+
+
+// Det er jo bare gas. Og hvis det er for grovt, så må du sige det... <3 Jeg mener det i kærlig ånd <3
