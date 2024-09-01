f1163e951f4e16292644ac9cf0810a2cc1c58752
Rosetalbo <roseoet@gmail.com>
2024-09-01 11:26:51 +0200

    Rose js

--- js/scripts.js
+++ js/scripts.js
@@ -113,3 +113,40 @@ function moveHandle(controlWidth, handlePos) {
         img360Image.alt = "McLaren 360 view image number " + Math.round(handlePos / frameDivider + 1);
     }
 }
+
+/*Rose*/
+console.log ("jeg virker")
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+const track = document.querySelector('.track');
+
+let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+
+window.addEventListener('resize' , () => {
+    carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+}) 
+
+let index = 0;
+
+next.addEventListener('click', () => {
+    index++;
+    prev.classList.add('show');
+    track.style.transform = `translateX(-${carouselWidth * index}px)`;
+
+    if (index == 4) {
+        next.classList.add('hide');
+    }
+
+});
+
+prev.addEventListener('click', () => {
+        index--;
+        next.classList.remove('hide');
+        if (index == 0) {
+            prev.classList.remove('show');
+        }
+        track.style.transform = `translateX(-${carouselWidth * index}px)`;
+    })
+
